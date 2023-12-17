af081b58a9834fec69e83390235c2af3d5a7614a
Dev Atul Patel <114083457+devvp8@users.noreply.github.com>
2023-12-17 14:11:40 +0530

    Update uninformed_search.py

--- uninformed_search.py
+++ uninformed_search.py
@@ -1,47 +1,59 @@
-graph = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['F'],
-    'D': [],
-    'E': ['F'],
-    'F': []
-}
-
-def dfs(graph, start, visited):
+def dfs(graph, start, visited=None, goal=None):
     if visited is None:
         visited = set()
     visited.add(start)
+    print(f"Visited: {visited}, Non-Visited: {set(graph.keys()) - visited}")
     print(start, end=' ')
+    if start == goal:
+        print(f"\nGoal state '{goal}' reached!")
+        return
     for neighbor in graph[start]:
         if neighbor not in visited:
-            dfs(graph, neighbor, visited)
+            dfs(graph, neighbor, visited, goal)
 
-def dfid(graph, start, depth, visited=None):
+def dfid(graph, start, depth, visited=None, goal=None):
     if visited is None:
         visited = set()
     if depth > 0:
         visited.add(start)
+        print(f"Visited: {visited}, Non-Visited: {set(graph.keys()) - visited}")
         print(start, end=' ')
+        if start == goal:
+            print(f"\nGoal state '{goal}' reached!")
+            return
         for neighbor in graph[start]:
             if neighbor not in visited:
-                dfid(graph, neighbor, depth - 1, visited)
+                dfid(graph, neighbor, depth - 1, visited, goal)
 
-def bfs(graph, start):
+def bfs(graph, start, goal=None):
     visited = set()
     queue = []
     queue.append(start)
     visited.add(start)
     while queue:
         node = queue.pop(0)
+        print(f"Visited: {visited}, Non-Visited: {set(graph.keys()) - visited}")
         print(node, end=' ')
+        if node == goal:
+            print(f"\nGoal state '{goal}' reached!")
+            return
         for neighbor in graph[node]:
             if neighbor not in visited:
                 visited.add(neighbor)
                 queue.append(neighbor)
 
-print("\nBFS traversal:")
-bfs(graph, 'A')
-print("\nDFID traversal:")
-dfid(graph, 'A', 2)
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': [],
+    'F': []
+}
+
+# print("\nBFS traversal:")
+# bfs(graph, 'A', 'C')
+# print("\nDFID traversal:")
+# dfid(graph, 'A', 2, goal='F')
 print("\nDFS traversal:")
-dfs(graph, 'A',None)
+dfs(graph, 'A', goal='F')
